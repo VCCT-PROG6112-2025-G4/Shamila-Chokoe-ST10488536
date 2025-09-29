8c29bda7c131ab05ac6194cabced1d761863c6c1
Shamila22-git <ST10488536@vcconnect.edu.za>
2025-09-29 13:04:17 +0200

    fht

---
+++ src/st10488536/test1/ST10488536Test1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package st10488536.test1;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class ST10488536Test1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
